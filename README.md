81365470a9876f16a29811f17727bb8f826eea3d
The Stig <thestig294@gmail.com>
2025-07-23 15:44:40 +0930

    Initial project setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Fabric-MC-Tournament-1.20.4
+A Fabric mod for 1.20.4 adding in-depth minigames for a Minecraft tournament with friends!
+
+## Credits
+Icons from https://game-icons.net\
+Laurels trophy icon by Delapouite under https://creativecommons.org/licenses/by/3.0/
---
+++ src/main/java/net/thestig294/mctournament/MCTournamentClient.java
@@ -0,0 +1,11 @@
+package net.thestig294.mctournament;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCTournamentClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        MCTournament.LOGGER.info("Hello client world!");
+    }
+}
